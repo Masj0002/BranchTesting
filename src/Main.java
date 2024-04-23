2ade0a31547a1eb29cf636bea7167ae52631fee9
Masj0002 <Masj0002@stud.kea.dk>
2024-04-23 14:34:53 +0200

    lyd

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dawg = new Dog();
+        dawg.makeSound();
+        Cat cawt = new Cat();
+        cawt.makeSound();
+    }
+}
